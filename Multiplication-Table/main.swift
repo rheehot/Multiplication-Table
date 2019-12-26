9d09c7544bee1a8fb810672a6df2444551ac88cd
corykim0829 <corykim0829@gmail.com>
2019-12-26 17:09:54 +0900

    Set up Xcode project

---
+++ Multiplication-Table/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Multiplication-Table
+//
+//  Created by Cory Kim on 2019/12/26.
+//  Copyright © 2019 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
